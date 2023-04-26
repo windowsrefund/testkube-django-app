627b4f72da11ca2c94e4a178fbd347b4bbd1e75e
richamishra <richamishra614@gmail.com>
2023-04-26 11:25:40 +0530

    added django app

---
+++ myproject/main_test.go
@@ -0,0 +1,31 @@
+package main_test
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	url := "http://localhost:30196"
+
+	res, err := http.Get(url)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code error: expected %d but got %d", http.StatusOK, res.StatusCode)
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !strings.Contains(string(body), "The install worked successfully! Congratulations") {
+		t.Errorf("Response body does not contain expected string")
+	}
+}
